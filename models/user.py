fcfc2f2dac76eaaab2c383c8cf7ea223a806be3b
ZwingliCaleb <calebzwingli@gmail.com>
2023-04-02 05:11:07 -0700

    add user.py

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    User class that inherits from BaseModel.
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+        Representation of a User.
+    """
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+
